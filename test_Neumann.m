fd818ab245262d7b502cfce74772ab428be6acce
snytav <snytav@gmail.com>
2020-10-17 00:57:06 +0700

    All the arrays being read from files set as parameters, read them from testbench script

---
+++ test_Neumann.m
@@ -0,0 +1,27 @@
+b = dlmread('b_3.txt');
+
+fr1 = dlmread('fr1.txt');
+fr2 = dlmread('fr2.txt');
+
+fz1 = dlmread('fz1.txt');
+fz2 = dlmread('fz2.txt');
+lm = size(fr1,1)-1;
+im = size(fz1,1)-2;
+
+b = reshape(b,im+2,lm+1);
+
+a = dlmread('hxhz.txt');
+hx = a(2);
+hz = a(3);
+
+left = dlmread('l.txt');
+cent = dlmread('c.txt');
+righ = dlmread('r.txt');
+
+u = neumannsolver(hx,hz,left,cent,righ,fr1,fr2,fz1,fz2,b);
+
+uf = dlmread('uf.txt');
+u1 = reshape(uf,im+2,lm+1);
+u2 = reshape(u',(im+2)*(lm+1),1);
+
+[m,i] = max(abs(u2-uf));
